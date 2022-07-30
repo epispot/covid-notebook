c5462af327071bf82cf088364b0c40f4c707d8c4
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-07-30 15:41:19 -0700

    Add territories dropdown

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import numpy as np
 import pandas as pd
 import epispot as epi
 import plotly.graph_objects as go
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 
 
 # create app
@@ -50,11 +50,75 @@ fig = go.Figure(go.Choroplethmapbox(
 fig.update_layout(
     mapbox_style='open-street-map',
     mapbox_zoom=3,
-    mapbox_center = {'lat': 37.0902, 'lon': -95.7129},
+    mapbox_center={'lat': 37.0902, 'lon': -95.7129},
 )
 fig.update_layout(
     margin={ 'r': 0, 't': 0, 'l': 0, 'b': 0 }
 )
+
+
+# callbacks
+@app.callback(
+    Output('graph', 'figure'),
+    Input('map-dropdown', 'value')
+)
+def change_map_view(value):
+    """Change map center and zoom depending on selection"""
+
+    center = {'lat': 37.0902, 'lon': -95.7129}
+    zoom = 3
+
+    match value:
+        case 'Contiguous U.S.':
+            pass
+        case 'Alaska':
+            center = {'lat': 63.3850, 'lon': -152.2683}
+            zoom = 2
+        case 'Hawaii':
+            center = {'lat': 19.8983, 'lon': -155.5822}
+            zoom = 5
+        case 'Puerto Rico & the U.S. Virgin Islands':
+            center = {'lat': 18.4389, 'lon': -66.0079}
+            zoom = 6.5
+        case 'Northern Mariana Islands':
+            center = {'lat': 15.2, 'lon': 145.75}
+            zoom = 7
+
+    # get data
+    df = core.find.data()
+    counties = core.get.counties()
+
+    # create main figure
+    fig = go.Figure(go.Choroplethmapbox(
+        geojson=counties, 
+        locations=df.fips, 
+        z=df.p_cases,
+        zmin=0, zmax=0.5,
+        colorscale=[
+            [0, '#adffc2'],
+            [0.5, '#ff9382'],
+            [1, '#c90061']
+        ],
+        marker_line_width=0,
+        marker_opacity=0.75,
+        text=
+            df.county + ', ' + df.state
+            + '<br>cases: '
+                + np.round(100 * df.p_cases, 1).astype(str) + '%'
+            + '<br>deaths: '
+                + np.round(100 * df.p_deaths, 1).astype(str) + '%',
+        hoverinfo='text',
+    ))
+    fig.update_layout(
+        mapbox_style='open-street-map',
+        mapbox_zoom=zoom,
+        mapbox_center=center,
+    )
+    fig.update_layout(
+        margin={ 'r': 0, 't': 0, 'l': 0, 'b': 0 }
+    )
+
+    return fig
 
 
 # create app layout
@@ -63,6 +127,11 @@ app.layout = html.Div(children=[
     html.Div(children='''
         An interactive notebook for examining trends in COVID-19 cases
     ''', id='subtitle'),
+    dcc.Dropdown([
+        'Contiguous U.S.', 'Alaska', 'Hawaii', 
+        'Puerto Rico & the U.S. Virgin Islands', 
+        'Northern Mariana Islands'
+    ], 'Contiguous U.S.', id='map-dropdown'),
     dcc.Graph(
         id='graph',
         figure=fig
